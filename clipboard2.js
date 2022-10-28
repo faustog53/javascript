1797a7a0078c106b3ebd7e1ce4f42bcedcb0a841
faustog53 <fausto.gambardella@gmail.com>
2022-10-28 20:37:30 +0200

    javascript clipboard

---
+++ clipboard2.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+   console.log("Hello World!");
+ });
+      document.addEventListener('DOMContentLoaded',function(){
+            let pasteButton = document.getElementsByTagName('button')[0];
+            pasteButton.addEventListener('click', function () {
+                navigator.clipboard
+                    .readText()
+                    .then(
+                        cliptext =>
+                            (document.getElementById('clipboard-paste').innerText = cliptext),
+                            err => console.log(err)
+                    );
+            });
+   
